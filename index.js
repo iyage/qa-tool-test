a3d81af6dfea982174bbdccfce012f47fd562b4c
yageagold <godwin.ofunrein@agoldresourcing.co.uk>
2026-03-27 20:13:34 +0100

    add try/catch for exceptions

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/users', async (_req, res) => {
-  const [rows] = await pool.query('SELECT id, email FROM users');
+  try {
+    const [rows] = await pool.query('SELECT id, email FROM users');
   return res.status(200).json(rows);
+
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+
 });
 
 app.post('/api/login', async (req, res) => {
