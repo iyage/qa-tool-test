eb4b20cfa2321d65ecb13d92ee7cfa38a3ecc497
yageagold <godwin.ofunrein@agoldresourcing.co.uk>
2026-03-27 21:08:58 +0100

    catch exception work

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ app.get('/api/users', async (_req, res) => {
 });
 
 app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ message: 'Email and password are required!' });
   }
-  try {
   const [rows] = await pool.query(
     'SELECT id FROM users WHERE email = ? AND password = ?',
     [email, password]
