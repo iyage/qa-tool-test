350c4a11db13b389f3bc0c0a56a4f6f29b97aef3
yageagold <godwin.ofunrein@agoldresourcing.co.uk>
2026-04-02 02:53:40 +0100

    remove error management

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/users', async (_req, res) => {
-  try {
+  // try {
     const users = await userService.getAllUsers();
     return res.status(200).json(users);
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
+  // } catch (error) {
+  //   console.error('Error fetching users:', error);
+  //   return res.status(500).json({ message: 'Internal server error' });
+  // }
 });
 
 app.get('/api/users/id/:id', async (req, res) => {
